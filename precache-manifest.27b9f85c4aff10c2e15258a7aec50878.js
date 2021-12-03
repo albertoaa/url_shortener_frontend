cc5192005d8e007ffd3a0b520c812c357c287b7b
Alberto Aragon Alvarez <ronin8503@gmail.com>
2021-12-03 13:39:12 +0000

    Updates

---
+++ precache-manifest.27b9f85c4aff10c2e15258a7aec50878.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b54f6be1f8626ebcbaaa6e42908d104",
+    "url": "/url_shortener_frontend/index.html"
+  },
+  {
+    "revision": "9d1bdb532ccdcf51db84",
+    "url": "/url_shortener_frontend/static/css/main.40b2829d.chunk.css"
+  },
+  {
+    "revision": "1204d42c2948e560cd8b",
+    "url": "/url_shortener_frontend/static/js/2.813140ff.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/url_shortener_frontend/static/js/2.813140ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d1bdb532ccdcf51db84",
+    "url": "/url_shortener_frontend/static/js/main.d6eea099.chunk.js"
+  },
+  {
+    "revision": "0688f5365dea12c1d0cc",
+    "url": "/url_shortener_frontend/static/js/runtime-main.8d21580f.js"
+  }
+]);
